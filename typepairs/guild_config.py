aa67267e5b2b04133d62686ede1deee7893e127a
Thomas Stemler <stemlertho@gmail.com>
2025-05-12 10:20:24 -0500

    Added starting_pay to guild configs

--- typepairs/guild_config.py
+++ typepairs/guild_config.py
@@ -6,10 +6,11 @@ class GuildConfig:
 
     AFFLICTION_CHANCE = 25  # Default chance for afflictions
 
-    def __init__(self, species: str, chance: int = AFFLICTION_CHANCE, minor_chance: int = AFFLICTION_CHANCE + 10):
+    def __init__(self, species: str, chance: int = AFFLICTION_CHANCE, minor_chance: int = AFFLICTION_CHANCE + 10, starting_pay = 100):
         self.species = species
         self.chance = chance
         self.minor_chance = minor_chance
+        self.starting_pay = starting_pay
 
     def __str__(self):
         return f"{self.species.title()} ({self.chance}%)"
@@ -20,7 +21,8 @@ class GuildConfig:
         return cls(
             species=data.get("species", ""),
             chance=data.get("chance", cls.AFFLICTION_CHANCE),
-            minor_chance=data.get("minor_chance", cls.AFFLICTION_CHANCE + 10)
+            minor_chance=data.get("minor_chance", cls.AFFLICTION_CHANCE + 10),
+            starting_pay = data.get("starting_pay", 100)
         )
 
 
@@ -30,6 +32,7 @@ class GuildConfigEncoder(json.JSONEncoder):
             return {
                 "species": obj.species,
                 "chance": obj.chance,
-                "minor_chance": obj.minor_chance
+                "minor_chance": obj.minor_chance,
+                "starting_pay": obj.starting_pay
             }
         return json.JSONEncoder.default(self, obj)
