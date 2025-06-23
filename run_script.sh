c4fbd11a6c2b945a062d265d1872fadd2382a26f
Thomas Stemler <stemlertho@gmail.com>
2025-06-23 14:21:37 -0500

    Added some logging to the bash script

--- run_script.sh
+++ run_script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+echo "Navigating to Pagget directory"
 cd /home/tommy/Dev/Pagget || exit
 # Pulling newest version
+echo "Pulling latest version..."
 git pull origin main
+echo ""
+echo "Running the bot"
 source .venv/bin/activate
 python main.py
 exec bash  # Keep terminal open
