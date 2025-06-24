eff28d0cf3f5fd5b1dccc06d9f26a879faeca71f
Thomas Stemler <stemlertho@gmail.com>
2025-06-24 16:07:04 -0500

    Added birth defect property

--- classes/typepairs/affliction.py
+++ classes/typepairs/affliction.py
@@ -5,11 +5,13 @@ from typing import Literal
 class Affliction:
     """Class representing an affliction with name, description, and rarity."""
 
-    def __init__(self, name: str, description: str, rarity: str, is_minor: bool = False, season: Literal["wet", "dry", None] = None):
+    def __init__(self, name: str, description: str, rarity: str, is_minor: bool = False, is_birth_defect: bool = False,
+                 season: Literal["wet", "dry", None] = None):
         self.name = name
         self.description = description
         self.rarity = rarity
         self.is_minor = is_minor
+        self.is_birth_defect = is_birth_defect
         self.season = season
 
     def __str__(self):
@@ -23,6 +25,7 @@ class Affliction:
             description=data.get("description", ""),
             rarity=data.get("rarity", ""),
             is_minor=data.get("is_minor", False),
+            is_birth_defect=data.get("is_birth_defect", False),
             season=data.get("season", None),
         )
 
@@ -34,7 +37,8 @@ class AfflictionEncoder(json.JSONEncoder):
                 "name": obj.name,
                 "description": obj.description,
                 "rarity": obj.rarity,
-                "is_minor": obj.is_minor, 
+                "is_minor": obj.is_minor,
+                "is_birth_defect": obj.is_birth_defect,
                 "season": obj.season,
             }
         return json.JSONEncoder.default(self, obj)
